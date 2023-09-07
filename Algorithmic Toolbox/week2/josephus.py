a9dc52ddf8eef9b7af2d0dc9be86488d6a2c2a44
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-09-07 16:52:21 +0600

    Update josephus.py

--- Algorithmic Toolbox/week2/josephus.py
+++ Algorithmic Toolbox/week2/josephus.py
@@ -1,40 +1,40 @@
 # Faster 
 
-# def josephus(n, k):
-#     if n == 1:
-#         return 0
-#     else:
-#         return (josephus(n - 1, k) + k) % n
+def josephus(n, k):
+    if n == 1:
+        return 0
+    else:
+        return (josephus(n - 1, k) + k) % n # (Josephus(n,k)−k)modn=Josephus(n−1,k) .
 
-# # Example usage:
-# n = 13  
-# k = 3  
-# survivor_position = josephus(n, k)
-# print("Position of the survivor:", survivor_position)
+# Example usage:
+n = 13  
+k = 3  
+survivor_position = josephus(n, k)
+print("Position of the survivor:", survivor_position)
 
 
 # another to solve and naive : 
-def josephus(n, k):
-    # Allocate a Boolean array to track who is alive or dead
-    alive_or_dead = [True] * n
-    number_of_survivors = n
-    current_position = 0
-    index = 0
+# def josephus(n, k):
+#     # Allocate a Boolean array to track who is alive or dead
+#     alive_or_dead = [True] * n
+#     number_of_survivors = n
+#     current_position = 0
+#     index = 0
 
-    while True:
-        if alive_or_dead[current_position]:
-            index += 1
-        if index == k:
-            number_of_survivors -= 1
-            if number_of_survivors == 0:
-                return current_position
-            else:
-                alive_or_dead[current_position] = False
-                index = 0
-        current_position = (current_position + 1) % n
+#     while True:
+#         if alive_or_dead[current_position]:
+#             index += 1
+#         if index == k:
+#             number_of_survivors -= 1
+#             if number_of_survivors == 0:
+#                 return current_position
+#             else:
+#                 alive_or_dead[current_position] = False
+#                 index = 0
+#         current_position = (current_position + 1) % n
 
-# Example usage:
-n = 13  # Number of rebels
-k = 3   # Every 3rd rebel is eliminated
-survivor_position = josephus(n, k)
-print("Position of the survivor:", survivor_position)
+# # Example usage:
+# n = 13  # Number of rebels
+# k = 3   # Every 3rd rebel is eliminated
+# survivor_position = josephus(n, k)
+# print("Position of the survivor:", survivor_position)
